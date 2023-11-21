9a1ff75ccc5d3f9b8b31a3264af94e0ec0d4a8ee
LeandrodoPrado <leandrodoprado@gmail.com>
2023-11-20 21:53:39 -0300

    '20.11'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_fisico/pages/edit_page.dart';
 
 import 'pages/second_page.dart';
 
@@ -42,6 +43,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    const double imageRadius = 40;
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -52,15 +55,46 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           child: Column(
             children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                  ),
-                  Text(
-                    'Lucas Anaissi',
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: imageRadius,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage(
+                        'assets/images/foto_perfil.jpg',
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Leandro do Prado',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (_) => EditPage(),
+                            );
+                          },
+                          child: Text(
+                            'Editar Perfil',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -109,7 +143,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-drawerButton(context, 'Configurações', Icon(Icons.settings)),
 Widget drawerButton(
   BuildContext context,
   String label,
